9868d71cd8141b32895c03bf447f5b09845b33a8
Ryan Barrett <ryan.barrett@spookyhouse.org>
2020-12-17 17:37:03 -0800

    cleanup

--- config/default.js
+++ config/default.js
@@ -2,11 +2,6 @@ const GOOGLE_CLOUD_KEY = process.env.GOOGLE_CLOUD_KEY;
 const SPOONACULAR_API_KEY = process.env.SPOONACULAR_API_KEY;
 
 module.exports = {
-  postgresUrl: process.env.POSTGRES_URL || '',
-  jwt: {
-    publicKey: process.env.PUBLIC_KEY,
-    privateKey: process.env.PRIVATE_KEY,
-  },
   googlePlaces: {
     getGroceryUrl: `https://maps.googleapis.com/maps/api/place/nearbysearch/json?radius=3000&type=grocery&key=${GOOGLE_CLOUD_KEY}&keyword=grocery`,
     getPhotoUrl: `https://maps.googleapis.com/maps/api/place/photo?key=${GOOGLE_CLOUD_KEY}&maxwidth=600&photoreference=`,
